628f9cf046ef16a85bc8ed3951b8a348756afc2c
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-30 21:30:23 +0530

    Mentor Management project

---
+++ Mentor-service/src/test/java/com/management/metorservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.metorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
